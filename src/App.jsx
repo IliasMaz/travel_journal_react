4cf1a9dc0ef1550aaed863aed1c8aa9550a98d27
IliasMaz <162236667+IliasMaz@users.noreply.github.com>
2025-11-28 00:37:56 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header.jsx";
+import Entry from "./components/Entry.jsx";
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <Entry />
+        <Entry />
+      </main>
+    </>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import globeImage from "../assets/global2.svg";
+import "../index.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="div-header">
+        <img src={globeImage} className="img-settings" alt="react-image" />
+        <h1>my travel journal.</h1>
+      </div>
+    </header>
+  );
+}
